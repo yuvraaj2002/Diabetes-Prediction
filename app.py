9515f3d822ff1711782bdbbd001ffe09c7581b59
Yuvraj Singh <ys2002github@gmail.com>
2023-07-07 19:28:55 +0530

    Added code to make function call and return a df

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import streamlit as st
 import pandas as pd
+from src.components.predict_pipe import Predict_Pipeline
 
 
 # Create a title and a header
 st.title("Diabetes Prediction Model")
 
 # Show the user input
-st.sidebar.write("This application based on your input data will help to predict that whether a person is having a diabetes or not.")
+st.sidebar.write(
+    "This application based on your input data will help to predict that whether a person is having a diabetes or not."
+)
+
 
 # Function to get user input
 def get_user_input():
@@ -16,29 +20,34 @@ def get_user_input():
     skin_thickness = st.sidebar.slider("Skin Thickness", 0, 99, 20)
     insulin = st.sidebar.slider("Insulin", 0, 846, 79)
     bmi = st.sidebar.slider("BMI", 0.0, 67.1, 31.9)
-    diabetes_pedigree = st.sidebar.slider("Diabetes Pedigree Function", 0.078, 2.42, 0.3725)
+    diabetes_pedigree = st.sidebar.slider(
+        "Diabetes Pedigree Function", 0.078, 2.42, 0.3725
+    )
     age = st.sidebar.slider("Age", 21, 81, 29)
 
     # Create a dictionary from the user input
     user_data = {
-        'Pregnancies': pregnancies,
-        'Glucose': glucose,
-        'Blood Pressure': blood_pressure,
-        'Skin Thickness': skin_thickness,
-        'Insulin': insulin,
-        'BMI': bmi,
-        'Diabetes Pedigree Function': diabetes_pedigree,
-        'Age': age
+        "Pregnancies": pregnancies,
+        "Glucose": glucose,
+        "Blood Pressure": blood_pressure,
+        "Skin Thickness": skin_thickness,
+        "Insulin": insulin,
+        "BMI": bmi,
+        "Diabetes Pedigree Function": diabetes_pedigree,
+        "Age": age,
     }
 
-    features = pd.DataFrame(user_data, index=[0])
-    return features
+    return user_data
+
+    # features = pd.DataFrame(user_data, index=[0])
+    # return features
 
 
 # Get user input
-user_input = get_user_input()
+user_input_dict = get_user_input()
+prediction_obj = Predict_Pipeline()
+user_input_df = prediction_obj.build_dataframe(user_input_dict)
 
 # Show the user input
 st.subheader("User Input:")
-st.write(user_input)
-
+st.write(user_input_df)
