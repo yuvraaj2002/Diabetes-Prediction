3abb3ebdd5c39f1081a35d63af8204aa974321df
Yuvraj Singh <ys2002github@gmail.com>
2023-07-07 17:50:08 +0530

    Added code to save the object

--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,10 @@ import pickle
 from src.exception import CustomException
 
 
+# This method will simply take the object and store it as pickle file
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
